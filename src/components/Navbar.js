a0341baead168e8ded8168bf5ddab037187f8408
priyanshu <buley2607@gmail.com>
2023-11-01 22:30:19 +0530

    Creating About Section

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div>
+      <Link to="/" style={{ margin: '30px' }}>
+        <b>Home</b>
+      </Link>
+      <Link to="/about">
+        <b>About</b>{' '}
+      </Link>
+    </div>
+  );
+}
+
+export default Navbar;
