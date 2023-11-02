6b50668b3b47e0565ac69d94db2a1c057c871465
priyanshu <buley2607@gmail.com>
2023-11-02 17:18:51 +0530

    creating navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="#main">Home </a>
+          </li>
+          <li>
+            <a href="#about">About </a>
+          </li>
+          <li>
+            <a href="#skill">Skill </a>
+          </li>
+          <li>
+            <a href="#service">Service </a>
+          </li>
+          <li>
+            <a href="#project">Project </a>
+          </li>
+          <li>
+            <a href="#contact">Contact </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
