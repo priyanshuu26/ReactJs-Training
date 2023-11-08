d6cf111c795dbdeeff01035ec9a3957bce3c2aef
priyanshu <buley2607@gmail.com>
2023-11-08 17:14:11 +0530

    Clean up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,28 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <a href="#main">Home </a>
-          </li>
-          <li>
-            <a href="#about">About </a>
-          </li>
-          <li>
-            <a href="#skill">Skill </a>
-          </li>
-          <li>
-            <a href="#service">Service </a>
-          </li>
-          <li>
-            <a href="#project">Project </a>
-          </li>
-          <li>
-            <a href="#contact">Contact </a>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <ul>
+        <li>
+          <a href="#main">Home </a>
+        </li>
+        <li>
+          <a href="#about">About </a>
+        </li>
+        <li>
+          <a href="#skill">Skill </a>
+        </li>
+        <li>
+          <a href="#service">Service </a>
+        </li>
+        <li>
+          <a href="#project">Project </a>
+        </li>
+        <li>
+          <a href="#contact">Contact </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
