51f5cf3fea4e7b57cf2f19ef54f2a617ff1cb892
priyanshu <buley2607@gmail.com>
2023-11-09 10:35:52 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import ProductDisplay from './components/ProductDisplay';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <ProductDisplay />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import Product from './Product'
-import { items } from '../constants/items'
-
+import React from 'react';
+import Product from './Product';
+import { items } from '../constants/items';
 
 function ProductDisplay() {
   return (
     <div>
-    {
-    items.map(item=>(
-        <Product 
-        title={item.name}
-        cost={item.price}
-        about={item.description}
-        />
-    ))
-    }
+      {items.map((item) => (
+        <Product title={item.name} cost={item.price} about={item.description} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductDisplay
+export default ProductDisplay;
