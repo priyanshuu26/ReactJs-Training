10363ae12492ebd191c178e95eef48086e24f555
priyanshu <buley2607@gmail.com>
2023-11-15 15:27:20 +0530

    add validations

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,58 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RegistrationForm() {
+  const [userRegistration, setUserRegistration] = useState({
+    name: '',
+    age: '',
+    gender: '',
+    programminglanguage: [],
+    username: '',
+    password: '',
+  });
+  const [records, setRecords] = useState([]);
+  const [language, setLanguage] = useState([]);
+  const [passwordError, setPasswordError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const handleInput = (e) => {
+    const { name, value, checked, type } = e.target;
+    if (type === 'checkbox') {
+      if (checked) {
+        setLanguage([...language, value]);
+      }
+    }
+    if (name === 'password' && value.length < 8) {
+      setPasswordError('Password should be minimum 8 characters');
+    } else {
+      setPasswordError('');
+      setUserRegistration({ ...userRegistration, [name]: value });
+    }
+    if (name === 'name' && !/^[a-zA-Z\s]+$/i.test(value)) {
+      setNameError('Invalid Name');
+    } else {
+      setNameError('');
+      setUserRegistration({ ...userRegistration, [name]: value });
+    }
+
+    setUserRegistration({ ...userRegistration, [name]: value });
+    console.log(`${name} : ${value} : ${checked}`);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newRecord = {
+      ...userRegistration,
+      programminglanguage: { ...language },
+    };
+    setRecords([...records, newRecord]);
+    console.log(language);
+  };
   return (
     <div>
       <h1>Registration From</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleInput}
+            value={userRegistration.name}
+          />
+          <div className="error">{nameError}</div>
         </div>
         <div>
           <label htmlFor="age">Age</label>
-          <input type="number" name="age" id="age" />
+          <input
+            type="number"
+            name="age"
+            id="age"
+            onChange={handleInput}
+            value={userRegistration.age}
+          />
         </div>
         <div>
           Gender :
-          <input type="radio" name="gender" id="male" value="male" />
-          <label htmlFor="male">Male</label>
-          <input type="radio" name="gender" id="female" value="female" />
-          <label htmlFor="male">Female</label>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            onChange={handleInput}
+          />
+          Male
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            onChange={handleInput}
+          />
+          Female
         </div>
         <div>
           Programming Languages:
-          <input type="checkbox" id="c" name="Programming Language" value="c" />
-          <label htmlFor="Programming Language">C</label>
           <input
             type="checkbox"
-            id="java"
-            name="Programming Language"
+            name="programminglanguage"
+            value="c"
+            onChange={handleInput}
+          />
+          <label htmlFor="programminglanguage">C</label>
+          <input
+            type="checkbox"
+            name="programminglanguage"
             value="java"
+            onChange={handleInput}
           />
-          <label htmlFor="Programming Language">Java</label>
+          <label htmlFor="programminglanguage">Java</label>
           <input
             type="checkbox"
-            id="Python"
-            name="Programming Language"
-            value="Python"
+            name="programminglanguage"
+            value="python"
+            onChange={handleInput}
           />
-          <label htmlFor="Programming Language">Python</label>
+          <label htmlFor="programminglanguage">Python</label>
           <input
             type="checkbox"
-            id="javascript"
-            name="Programming Language"
+            name="programminglanguage"
             value="javascript"
+            onChange={handleInput}
           />
-          <label htmlFor="Programming Language">Javascript</label>
+          <label htmlFor="programminglanguage">Javascript</label>
         </div>
         <div>
           <label htmlFor="username">username</label>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={handleInput}
+            value={userRegistration.username}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handleInput}
+            value={userRegistration.password}
+          />
+          <div className="error">{passwordError}</div>
         </div>
         <div>
           <button type="submit">Submit</button>
