6abd53bc2e752a771bfef70b1b2db0f1baf1d65e
priyanshu <buley2607@gmail.com>
2023-11-17 17:24:14 +0530

    add toggle button

--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import productList from '../constants/ProductList';
 
 function ProductDisplay() {
+  const [index, setIndex] = useState(0);
+  const totalPageNumber = productList.length;
+  const handleIncrement = () => {
+    setIndex((prevIndex) => (prevIndex + 1) % totalPageNumber);
+  };
+
+  const handleDecrement = () => {
+    setIndex(
+      (prevIndex) => (prevIndex - 1 + totalPageNumber) % totalPageNumber
+    );
+  };
   return (
     <div>
-      {productList.map((data) => (
-        <li key={data.id}>
-          <h2>{data.name}</h2>
-          <h3>catagory: {data.category}</h3>
-          <h5>Price: {data.price}</h5>
-        </li>
-      ))}
+      <h1>Product's Name = {productList[index].name}</h1>
+      <h3>Product's Category = {productList[index].category}</h3>
+      <h5>Product's Price = {productList[index].price} $</h5>
+      <div className="button">
+        <button type="button" onClick={handleDecrement}>
+          &lt;
+        </button>
+        <p>
+          {index} of {totalPageNumber}
+        </p>
+        <button type="button" onClick={handleIncrement}>
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
